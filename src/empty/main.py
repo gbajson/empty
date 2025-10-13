cd955270dc8703e1ac68f09a85edc0b72759fa7a
Grzegorz Bajson <gbajson@protonmail.ch>
2025-10-13 09:41:15 +0200

    WIP

--- src/empty/main.py
+++ src/empty/main.py
@@ -1,6 +1,46 @@
 """Empty module."""
 
 import asyncio
+from statistics import median
+from typing import Any, Protocol, Sequence, TypeVar
+
+
+class Comparable(Protocol):  # pylint: disable=too-few-public-methods
+    """Protocol for Comparable."""
+
+    def __lt__(self, other: Any, /) -> bool: ...
+    def __le__(self, other: Any, /) -> bool: ...
+
+
+T = TypeVar("T", bound=Comparable)
+
+
+def append_sorted(l1: Sequence[T], l2: Sequence[T]) -> list[T]:
+    """Appends two sorted lists."""
+    idx_1 = 0
+    idx_2 = 0
+    len_1 = len(l1)
+    len_2 = len(l2)
+    n: list[T] = []
+
+    for _ in range(len_1 + len_2):
+        if idx_1 == len_1:
+            n += l2[idx_2:]
+            break
+        if idx_2 == len_2:
+            n += l1[idx_1:]
+            break
+
+        x = l1[idx_1]
+        y = l2[idx_2]
+
+        if x <= y:
+            n.append(x)
+            idx_1 += 1
+        else:
+            n.append(y)
+            idx_2 += 1
+    return n
 
 
 class Solution:
@@ -49,6 +89,23 @@ class Solution:
             last_index[letter] = idx
             max_len = max(max_len, idx - left + 1)
         return max_len
+
+    def findMedianSortedArrays(  # pylint: disable=invalid-name
+        self, nums1: list[int], nums2: list[int]
+    ) -> float:
+        """https://leetcode.com/problems/median-of-two-sorted-arrays/"""
+        return float(median(nums1 + nums2))
+
+    def findMedianSortedArrays_append(  # pylint: disable=invalid-name
+        self, nums1: list[int], nums2: list[int]
+    ) -> float:
+        """https://leetcode.com/problems/median-of-two-sorted-arrays/"""
+        nums: list[int] = append_sorted(nums1, nums2)
+
+        total = len(nums)
+        m = total // 2
+
+        return nums[m] if total % 2 else (nums[m] + nums[m - 1]) / 2
 
 
 async def add(n: float, m: float) -> float:
