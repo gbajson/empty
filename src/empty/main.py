ad17e39b97535ae575e96986adf2da6b09c04faf
Grzegorz Bajson <gbajson@protonmail.ch>
2025-10-13 17:24:42 +0200

    WIP

--- src/empty/main.py
+++ src/empty/main.py
@@ -8,10 +8,95 @@ async def add(n: float, m: float) -> float:
     return n + m
 
 
+# def pair(s: str):
+#     a = s.split(",")
+#     result = []
+#     l = len(a)
+#     idx = 0
+#     while idx < l:
+#         if idx + 1 < l:
+#             value = (
+#                 int(a[idx]),
+#                 int(a[idx + 1]),
+#             )
+#             result.append(value)
+#         idx += 2
+
+#     return result
+
+
+# # n zarowek (wl, wyl)
+
+
+# def switch(z, x):
+#     l = len(z)
+#     if x < 0:
+#         return z
+
+#     for y in range(x):
+#         idx = 0
+#         while idx < l:
+#             z[idx] = not (z[idx])
+#             idx += y + 1
+#     return z
+
+
 async def main() -> None:
     """Main."""
-    print("Hello from empty!")
+    # a = [1, 2]
+
+    # assert pair("1, 2, 3, 4") == [(1, 2), (3, 4)]
+    # print("ok")
+
+    # n = 100
+    # z = [False] * n
+    # switch(z, 100)
+    # print(z[73])
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
+# Movies ( mID, title, year )
+
+# Reviewers ( rID, name )
+
+# Ratings ( rID, mID, score )
+
+
+# Movies
+# mID	title	    year
+# -----|---------------
+# 11	Star Wars	1977
+# 22	The Mask	1994
+
+
+# Reviewers
+# rID	name
+# -----|----------------
+# 21	Sarah Martinez
+# 22	Daniel Lewis
+
+
+# Ratings
+# rID	mID	score
+# ---|-------|
+# 21	11	9
+
+
+# Znajdz wszytkie filmy nie oceniane przez Daniel Lewis
+# select * from movies m
+#  join Reviewers rev
+#  outer join ratings rat
+# where m.mId = ratings.mID
+# minus
+# select * from movies
+#  join Reviewers rev
+#  outer join ratings rat
+# where ratings.rId = rev.rId;
+
+
+# Godziny pracy
+# 10.30 -- 11.00
+# 15:30 --
